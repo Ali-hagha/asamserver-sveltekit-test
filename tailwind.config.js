f26831afd2a84a126ab95f904eb60be086235418
Ali home pc <ali.haghayegh@gmail.com>
2024-02-04 21:27:05 +0330

    add project colors to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./src/**/*.{html,js,svelte,ts}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {
+			colors: {
+				slate_gray: '#717E8B',
+				porcelain: '#EDEFF1',
+				french_gray: '#BCC0C4',
+				stone_gray: '#7D848A',
+				accent_green: '#2BAB89',
+				accent_light_green: '#E9F7F3',
+				accent_red: '#FF6B6B',
+				accent_light_red: '#FFEEEE',
+				space_gray: '#49535B',
+				charcoal: '#2B4053',
+				primary_dark: '#13283E',
+				athens_gray: '#FCFCFD',
+				cadet_gray: '#9FADB9',
+				rich_black: '#06161A'
+			}
+		}
+	},
+	plugins: []
+};
