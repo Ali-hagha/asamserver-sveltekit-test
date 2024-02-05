5b4c3185d303b39b33ad946f38d97ac0b230ec6a
Ali home pc <ali.haghayegh@gmail.com>
2024-02-05 16:01:28 +0330

    add event handler prop to button component

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -3,19 +3,26 @@
 	export let title: Optional<string> = undefined;
 	export let icon: Optional<string> = undefined;
 	export let type: 'primary' | 'secondary' | 'icon';
+	export let clickHandler: (
+		e: MouseEvent & { currentTarget: EventTarget & HTMLButtonElement }
+	) => void = (e) => {
+		console.log(e.currentTarget);
+	};
 
 	$: isPrimary = type === 'primary';
 	$: isSecondary = type === 'secondary';
 	$: isIcon = type === 'icon';
 
-	$: primaryClasses = 'bg-accent_green hover:bg-accent_green/90 active:bg-accent_green text-white';
+	$: primaryClasses =
+		'px-9 bg-accent_green hover:bg-accent_green/90 active:bg-accent_green text-white';
 	$: secondaryClasses =
-		'bg-porcelain text-space_gray hover:bg-french_gray/60 active:bg-porcelain flex items-center justify-center gap-2.5';
+		'px-9 bg-porcelain text-space_gray hover:bg-french_gray/60 active:bg-porcelain flex items-center justify-center gap-2.5';
 	$: iconClasses = 'bg-porcelain text-space_gray hover:bg-french_gray/60 active:bg-porcelain px-4';
 </script>
 
 <button
-	class="py-4 px-9 rounded-[5px] font-medium transition-colors
+	on:click={clickHandler}
+	class="py-4 rounded-[5px] font-medium transition-colors
     {isPrimary ? primaryClasses : ''}
     {isSecondary ? secondaryClasses : ''}
     {isIcon ? iconClasses : ''}
