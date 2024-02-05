1bdfc943dc095f62eeb33efa7bddf719ca28f9a9
Ali home pc <ali.haghayegh@gmail.com>
2024-02-05 16:02:00 +0330

    create breadcrumbs component

---
+++ src/lib/components/Breadcrumbs.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import iconTag from '$lib/icons/tag-right.svg';
+
+	const invoiceId = 26773;
+
+	const paths = [
+		{
+			title: 'Dashboard',
+			href: '/'
+		},
+		{
+			title: 'my invoices',
+			href: '/'
+		},
+		{
+			title: `#${invoiceId}`,
+			href: '/'
+		}
+	];
+</script>
+
+<div class="flex gap-2 items-center">
+	<img src={iconTag} alt="tag" />
+	<div class="text-sm breadcrumbs">
+		<ul class="text-stone_gray">
+			{#each paths as path, i}
+				{#if i === paths.length - 1}
+					<li class="text-french_gray">{path.title}</li>
+				{:else}
+					<li><a href={path.href}>{path.title}</a></li>
+				{/if}
+			{/each}
+		</ul>
+	</div>
+</div>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -3,13 +3,15 @@
 	import iconWallet from '$lib/icons/vuesax-outline-wallet-add.svg';
 	import iconCart from '$lib/icons/cart.svg';
 	import Button from '$lib/components/Button.svelte';
+	import Breadcrumbs from '$lib/components/Breadcrumbs.svelte';
 </script>
 
 <header
 	class="ml-[135px] px-[50px] pt-12 pb-[60px] bg-athens_gray flex items-center justify-between"
 >
 	<div>
-		<h2 class="text-3xl mb-3">Invoice Details</h2>
+		<h2 class="text-3xl mb-3 text-primary_dark">Invoice Details</h2>
+		<Breadcrumbs />
 	</div>
 	<div class="flex gap-5">
 		<Button title="Deploy Now" type="primary" />
