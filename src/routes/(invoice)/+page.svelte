f084d3efea9f55070505e184d1435ca71fd96cc1
Ali home pc <ali.haghayegh@gmail.com>
2024-02-06 11:11:57 +0330

    move account credit selection to separate component

--- src/routes/(invoice)/+page.svelte
+++ src/routes/(invoice)/+page.svelte
@@ -1,11 +1,9 @@
 <script lang="ts">
-	import { currencyFormatter } from '$lib/util';
 	import type { PaymentMethod } from '$lib/types/types';
 
 	import PaymentAccordionItem from './PaymentAccordionItem.svelte';
 	import InvoiceHeader from './InvoiceHeader.svelte';
 
-	import iconCreditCard from '$lib/icons/card-pos.svg';
 	import iconBitcoin from '$lib/icons/crypto/Bitcoin.svg';
 	import iconEthereum from '$lib/icons/crypto/Ethereum.svg';
 	import iconLitecoin from '$lib/icons/crypto/Lightcoin.svg';
@@ -14,6 +12,7 @@
 	import iconStripe from '$lib/icons/payment/stripe.svg';
 	import iconDiscover from '$lib/icons/payment/discover.svg';
 	import iconWebmoney from '$lib/icons/payment/Webmoney.svg';
+	import AccountCredit from './AccountCredit.svelte';
 
 	export let data;
 	let checked: PaymentMethod = 'CRYPTO';
@@ -27,47 +26,8 @@
 		<div class="px-[15px] mb-5" data-theme="light">
 			<p class="font-medium">Payment Methods</p>
 		</div>
-		<div class="border-accent_green border rounded-[5px] ring-[5px] ring-accent_light_green mb-10">
-			<div class="border-accent_green border-b p-5 flex gap-4">
-				<img src={iconCreditCard} alt="credit card" />
-				<p class="font-medium text-accent_green">Apply Credit</p>
-			</div>
-			<div class="p-[30px] flex items-center justify-between">
-				<div class="border-l-2 border-l-porcelain pl-3 text-slate_gray leading-7 text-sm">
-					<p>
-						The amount in your account is <span class="font-semibold text-accent_green"
-							>{currencyFormatter.format(accountBalance)} USD.</span
-						>
-					</p>
-					<p>
-						Do you want this amount to be taken off <span class="font-semibold text-charcoal"
-							>this invoice</span
-						>?
-					</p>
-					<p>
-						Please type the number you want in the box below and click "<span
-							class="font-semibold text-accent_green">Apply</span
-						>"
-					</p>
-				</div>
-				<div
-					class="border border-porcelain rounded-[5px] text-stone_gray p-[5px] flex items-center justify-between"
-				>
-					<input
-						type="number"
-						min="0.01"
-						step="0.01"
-						max="2500"
-						placeholder="15"
-						class="p-0 w-24 border-0 focus:ring-0 focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
-					/>
-					<button
-						class="font-medium text-accent_green py-2 px-2.5 transition-colors
-						bg-accent_light_green hover:bg-green-100 active:bg-accent_light_green rounded-[5px]">Apply</button
-					>
-				</div>
-			</div>
-		</div>
+
+		<AccountCredit {accountBalance} />
 
 		<PaymentAccordionItem bind:checked value="CRYPTO">
 			<div slot="title" class="flex items-center justify-between flex-1">
---
+++ src/routes/(invoice)/AccountCredit.svelte
@@ -0,0 +1,48 @@
+<script lang="ts">
+	import { currencyFormatter } from '$lib/util';
+	import iconCreditCard from '$lib/icons/card-pos.svg';
+
+	export let accountBalance: number;
+</script>
+
+<div class="border-accent_green border rounded-[5px] ring-[5px] ring-accent_light_green mb-10">
+	<div class="border-accent_green border-b p-5 flex gap-4">
+		<img src={iconCreditCard} alt="credit card" />
+		<p class="font-medium text-accent_green">Apply Credit</p>
+	</div>
+	<div class="p-[30px] flex items-center justify-between">
+		<div class="border-l-2 border-l-porcelain pl-3 text-slate_gray leading-7 text-sm">
+			<p>
+				The amount in your account is <span class="font-semibold text-accent_green"
+					>{currencyFormatter.format(accountBalance)} USD.</span
+				>
+			</p>
+			<p>
+				Do you want this amount to be taken off <span class="font-semibold text-charcoal"
+					>this invoice</span
+				>?
+			</p>
+			<p>
+				Please type the number you want in the box below and click "<span
+					class="font-semibold text-accent_green">Apply</span
+				>"
+			</p>
+		</div>
+		<div
+			class="border border-porcelain rounded-[5px] text-stone_gray p-[5px] flex items-center justify-between"
+		>
+			<input
+				type="number"
+				min="0.01"
+				step="0.01"
+				max="2500"
+				placeholder="15"
+				class="p-0 w-24 border-0 focus:ring-0 focus:outline-none [appearance:textfield] [&::-webkit-outer-spin-button]:appearance-none [&::-webkit-inner-spin-button]:appearance-none"
+			/>
+			<button
+				class="font-medium text-accent_green py-2 px-2.5 transition-colors
+						bg-accent_light_green hover:bg-green-100 active:bg-accent_light_green rounded-[5px]">Apply</button
+			>
+		</div>
+	</div>
+</div>
